5375bb8b68e90740cccf64d02e839f76fe24f222
Roosterbear <lfpereag@gmail.com>
2019-11-20 08:40:25 -0600

    firmas de metodos

---
+++ firmaMetodo.java
@@ -0,0 +1,11 @@
+public class Persona {
+	/*
+    	La firma del método es:
+    	1.- El valor que regresa 
+    	2.- El nombre del método
+    	3.- Los tipos de argumentos
+    */
+    int Suma(int a, int b){
+        return a+b;
+    }
+}
