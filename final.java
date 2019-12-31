67933797050b301d765742e76269e5e8aeac4dd5
Luis Perea <lfpereag@gmail.com>
2019-12-31 13:13:37 -0600

    Final

---
+++ final.java
@@ -0,0 +1,56 @@
+public class Persona {
+    public static void  main(String args[]){
+        EjemploFinal ef = new EjemploFinal();
+        Nieto nieto = new Nieto();
+        System.out.println("Imprimiendo EJEMPLO FINAL");
+        System.out.println(ef.imprimir());
+        System.out.println(ef.imprimir2());
+        System.out.println("Imprimiendo NIETO");
+        System.out.println(nieto.imprimir());
+        System.out.println(nieto.imprimir2());
+    } 
+ }
+ 
+ final class Ultimo{
+     Boolean imprime(){
+         /*
+         // No podemos cambiarle el valor a un final
+         EjemploFinal.noMeCambies = true;
+         
+         */
+         
+         return EjemploFinal.noMeCambies;
+     }   
+ }
+ 
+ class EjemploFinal{
+     public static final Boolean noMeCambies = false;
+     
+     final String imprimir(){
+         return "No me puedes sobreescribir";
+     }
+     
+     String imprimir2(){
+         return "Haz de mi lo que quieras!";
+     }
+ }
+ 
+ class Nieto extends EjemploFinal{
+     /*
+     // No podemos sobreescribir un metodo final
+     String imprimir(){
+         
+     }
+     */
+     
+     String imprimir2(){
+         return "He sido cambiado";
+     }
+ }
+ /*
+ // Esta clase marcaria error porque no podemos heredar de una clase final
+ class Hijo extends Ultimo{
+     
+ }
+ */
+ 
