94ef25db6b2a24385fe873a4029332ac2452a8ba
Roosterbear <lfpereag@gmail.com>
2020-01-23 13:09:33 -0600

    package

---
+++ package.java
@@ -0,0 +1,14 @@
+package com.roosterbear.persona;
+
+import com.roosterbear.food.Fruit;
+
+public class prueba {
+    public static void main (String[] args){
+               
+        
+    }
+    
+    class Manzana extends Fruit{
+        
+    }
+}
